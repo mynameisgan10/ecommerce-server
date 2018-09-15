1edcb3000f8441a2168889c2034cf4950eed3b45
Gan <geekwithgun@gmail.com>
2018-09-15 17:03:28 +0700

    hashing password

--- controllers/authentication/authentication.js
+++ controllers/authentication/authentication.js
@@ -1,24 +1,30 @@
 const db = require('../../models/dbconnection');
+const bcrypt = require('bcrypt');
 
 const authentication = {
-    authenticate: (req,res) => {
-        res.json({
-            status: "success"
-        })
+    authenticate: (req, res) => {
+        res.json({status: "success"})
     },
-    signup: (req,res) => {
+    signup: (req, res) => {
         const user = {
             username: "testname",
             password: "123123123"
         }
-        db.query('INSERT INTO Users SET ? ', user, (error, results) => {
-            if (error) throw error
-            res.json({
-                status: "success",
-                message: "signed up"
+        bcrypt
+            .hash(user.password, 12)
+            .then(hash => {
+                user.password = hash;
+                db.query('INSERT INTO Users SET ? ', user, (error, results) => {
+                    if (error) 
+                        throw error
+                    res.json({status: "success", message: "signed up"})
+                })
             })
-        })
-    }
+            .catch(error => {
+                throw error;
+            })
+
+        }
 }
 
 module.exports = authentication;

07c537b26c7bac92897001c9d1c0ad7ffbaa3357
Gan <geekwithgun@gmail.com>
2018-09-21 18:54:26 +0700

    login and signup done

--- controllers/authentication/authentication.js
+++ controllers/authentication/authentication.js
@@ -5,42 +5,53 @@ const csrftoken = require('csrf');
 
 const authentication = {
     authenticate: (req, res) => {
+        console.log(req.body);
         db.query(
             'SELECT * FROM USERS WHERE username = ? LIMIT 1',
             [req.body.username],
             (err, results) => { //sign jwt and return later
-                if (err) 
-                    throw error;
+                if (err) {
+                    throw err;
+                }
+                console.log(results)
+                if (results.length === 0) {
+                    return res.json({success: false, message: "cannot find user"})
+                }
                 bcrypt
                     .compare(req.body.password, results[0].password)
                     .then(passwordStatus => {
-                        const csrf = new csrftoken();
-                        var secret = csrf.secretSync()
-                        var xsrf = csrf.create(secret)
-                        const token = jwt.sign({
-                            username: results[0].username,
-                            login: passwordStatus,
-                            xsrf: xsrf,
-                            secret: secret
-                        }, process.env.JWT_SECRET + results[0].password, { //uses our secret + the user's hash as the secret
-                            expiresIn: '1h'
-                        });
-                        const refreshToken = jwt.sign({
-                            username: results[0].username,
-                            login: passwordStatus
-                        }, process.env.JWT_REFRESH_SECRET + results[0].password, {expiresIn: '7d'})
-                        res.cookie("xsrf", xsrf, {expires: new Date(Date.now() + 900000)});
-                        res.cookie("token", token, {
-                            httpOnly: true,
-                            expires: new Date(Date.now() + 900000)
-                        });
-                        res.cookie("refreshtoken", refreshToken, {
-                            httpOnly: true
-                        });
-                        res.json({token: token, refreshToken: refreshToken});
+                        console.log("verifying");
+                        if (passwordStatus) {
+                            const csrf = new csrftoken();
+                            var secret = csrf.secretSync()
+                            var xsrf = csrf.create(secret)
+                            const token = jwt.sign({
+                                username: results[0].username,
+                                login: passwordStatus,
+                                xsrf: xsrf,
+                                secret: secret
+                            }, process.env.JWT_SECRET + results[0].password, { //uses our secret + the user's hash as the secret
+                                expiresIn: '1h'
+                            });
+                            const refreshToken = jwt.sign({
+                                username: results[0].username,
+                                login: passwordStatus
+                            }, process.env.JWT_REFRESH_SECRET + results[0].password, {expiresIn: '7d'})
+                            res.cookie("xsrf", xsrf, {
+                                expires: new Date(Date.now() + 900000)
+                            });
+                            res.cookie("token", token, {
+                                httpOnly: true,
+                                expires: new Date(Date.now() + 900000)
+                            });
+                            res.cookie("refreshtoken", refreshToken, {httpOnly: true});
+                            res.json({success: true, token: token, refreshToken: refreshToken});
+                        } else {
+                            res.json({success: false, message: "wrong password"})
+                        }
                     })
                     .catch(error => {
-                        throw error
+                        res.json({success: false, message: error.message})
                     })
 
                 }
@@ -48,29 +59,68 @@ const authentication = {
 
     },
     signup: (req, res) => {
-        // const user = {
-        //     username: req.body.username,
-        //     password: req.body.password
-        // }
+        console.log(req.body);
+        if (req.body.password.trim() !== req.body.confirmpassword.trim() || req.body.username.trim() === "") {
+            return res.json({success: false, message: "password not the same"})
+        }
         const user = {
-            username: "testname1",
-            password: "123123123"
+            username: req.body.username,
+            password: req.body.password,
+            confirmpassword: req.body.confirmpassword
         }
-        bcrypt
-            .hash(user.password, 12)
-            .then(hash => {
-                user.password = hash;
-                db.query('INSERT INTO Users SET ? ', user, (error, results) => {
-                    if (error) 
-                        throw error
-                    res.json({status: "success", message: "signed up"})
-                })
-            })
-            .catch(error => {
-                throw error;
-            })
 
-        }
+        db.query(
+            "SELECT * FROM USERS where username = ? LIMIT 1",
+            [req.body.username],
+            (err, results) => {
+                if (err) {
+                    throw err;
+                }
+                if (results.length !== 0) {
+                    console.log(results);
+                    res.json({success: false, message: "this username has already been taken"})
+                } else {
+                    bcrypt
+                        .hash(user.password, 12)
+                        .then(hash => {
+                            user.password = hash;
+                            db.query('INSERT INTO Users SET ? ', {username:user.username,password: user.password}, (error, results) => {
+                                if (error) 
+                                    throw error
+                                    const csrf = new csrftoken();
+                                    var secret = csrf.secretSync()
+                                    var xsrf = csrf.create(secret)
+                                    const token = jwt.sign({
+                                        username: user.username,
+                                        login: true,
+                                        xsrf: xsrf,
+                                        secret: secret
+                                    }, process.env.JWT_SECRET + user.password, { //uses our secret + the user's hash as the secret
+                                        expiresIn: '1h'
+                                    });
+                                    const refreshToken = jwt.sign({
+                                        username: user.username,
+                                        login: true
+                                    }, process.env.JWT_REFRESH_SECRET + user.password, {expiresIn: '7d'})
+                                    res.cookie("xsrf", xsrf, {
+                                        expires: new Date(Date.now() + 900000)
+                                    });
+                                    res.cookie("token", token, {
+                                        httpOnly: true,
+                                        expires: new Date(Date.now() + 900000)
+                                    });
+                                    res.cookie("refreshtoken", refreshToken, {httpOnly: true});
+                                    res.json({success: true, token: token, refreshToken: refreshToken});
+                            })
+                        })
+                        .catch(error => {
+                            throw error;
+                        })
+                    }
+            }
+        )
+
+    }
 }
 
 module.exports = authentication;
